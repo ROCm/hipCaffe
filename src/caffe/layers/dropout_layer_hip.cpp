15df1d6e5b169ffef9cf9a8bbe119bf218bdd8fb
Neelakandan <neelakandan@multicorewareinc.com>
2016-08-03 13:28:19 +0530

    Add deconv and dropout HIP implementation

--- src/caffe/layers/dropout_layer_hip.cpp
+++ src/caffe/layers/dropout_layer_hip.cpp
@@ -6,10 +6,10 @@
 namespace caffe {
 
 template <typename Dtype>
-__global__ void DropoutForward(const int n, const Dtype* in,
+__global__ void DropoutForward(hipLaunchParm lp, const int n, const Dtype* in,
     const unsigned int* mask, const unsigned int threshold, const float scale,
     Dtype* out) {
-  CUDA_KERNEL_LOOP(index, n) {
+  HIP_KERNEL_LOOP(index, n) {
     out[index] = in[index] * (mask[index] > threshold) * scale;
   }
 }
@@ -26,19 +26,19 @@ void DropoutLayer<Dtype>::Forward_gpu(const vector<Blob<Dtype>*>& bottom,
     caffe_gpu_rng_uniform(count, mask);
     // set thresholds
     // NOLINT_NEXT_LINE(whitespace/operators)
-    DropoutForward<Dtype><<<CAFFE_GET_BLOCKS(count), CAFFE_CUDA_NUM_THREADS>>>(
+    hipLaunchKernel(HIP_KERNEL_NAME(DropoutForward<Dtype>), dim3(CAFFE_GET_BLOCKS(count)), dim3(CAFFE_HIP_NUM_THREADS), 0, 0,
         count, bottom_data, mask, uint_thres_, scale_, top_data);
-    CUDA_POST_KERNEL_CHECK;
+   // HIP_POST_KERNEL_CHECK;
   } else {
     caffe_copy(count, bottom_data, top_data);
   }
 }
 
 template <typename Dtype>
-__global__ void DropoutBackward(const int n, const Dtype* in_diff,
+__global__ void DropoutBackward(hipLaunchParm lp, const int n, const Dtype* in_diff,
     const unsigned int* mask, const unsigned int threshold, const float scale,
     Dtype* out_diff) {
-  CUDA_KERNEL_LOOP(index, n) {
+  HIP_KERNEL_LOOP(index, n) {
     out_diff[index] = in_diff[index] * scale * (mask[index] > threshold);
   }
 }
@@ -55,10 +55,10 @@ void DropoutLayer<Dtype>::Backward_gpu(const vector<Blob<Dtype>*>& top,
           static_cast<const unsigned int*>(rand_vec_.gpu_data());
       const int count = bottom[0]->count();
       // NOLINT_NEXT_LINE(whitespace/operators)
-      DropoutBackward<Dtype><<<CAFFE_GET_BLOCKS(count),
-        CAFFE_CUDA_NUM_THREADS>>>(
-          count, top_diff, mask, uint_thres_, scale_, bottom_diff);
-      CUDA_POST_KERNEL_CHECK;
+      hipLaunchKernel(HIP_KERNEL_NAME(DropoutBackward<Dtype>), dim3(CAFFE_GET_BLOCKS(count)),
+        dim3(CAFFE_HIP_NUM_THREADS), 0, 0, 
+        count, top_diff, mask, uint_thres_, scale_, bottom_diff);
+      // HIP_POST_KERNEL_CHECK;
     } else {
       caffe_copy(top[0]->count(), top_diff, bottom_diff);
     }
