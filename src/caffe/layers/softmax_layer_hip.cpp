12beab586c263e8643b189169d353f942e567ab9
Neelakandan <neelakandan@multicorewareinc.com>
2016-08-04 09:26:49 +0530

    Add softmax hip impln

--- src/caffe/layers/softmax_layer_hip.cpp
+++ src/caffe/layers/softmax_layer_hip.cpp
@@ -2,17 +2,15 @@
 #include <cfloat>
 #include <vector>
 
-#include "thrust/device_vector.h"
-
 #include "caffe/layers/softmax_layer.hpp"
 #include "caffe/util/math_functions.hpp"
 
 namespace caffe {
 
 template <typename Dtype>
-__global__ void kernel_channel_max(const int num, const int channels,
+__global__ void kernel_channel_max(hipLaunchParm lp, const int num, const int channels,
     const int spatial_dim, const Dtype* data, Dtype* out) {
-  CUDA_KERNEL_LOOP(index, num * spatial_dim) {
+  HIP_KERNEL_LOOP(index, num * spatial_dim) {
     int n = index / spatial_dim;
     int s = index % spatial_dim;
     Dtype maxval = -FLT_MAX;
@@ -24,10 +22,10 @@ __global__ void kernel_channel_max(const int num, const int channels,
 }
 
 template <typename Dtype>
-__global__ void kernel_channel_subtract(const int count,
+__global__ void kernel_channel_subtract(hipLaunchParm lp, const int count,
     const int num, const int channels,
     const int spatial_dim, const Dtype* channel_max, Dtype* data) {
-  CUDA_KERNEL_LOOP(index, count) {
+  HIP_KERNEL_LOOP(index, count) {
     int n = index / channels / spatial_dim;
     int s = index % spatial_dim;
     data[index] -= channel_max[n * spatial_dim + s];
@@ -35,16 +33,16 @@ __global__ void kernel_channel_subtract(const int count,
 }
 
 template <typename Dtype>
-__global__ void kernel_exp(const int count, const Dtype* data, Dtype* out) {
-  CUDA_KERNEL_LOOP(index, count) {
+__global__ void kernel_exp(hipLaunchParm lp, const int count, const Dtype* data, Dtype* out) {
+  HIP_KERNEL_LOOP(index, count) {
     out[index] = exp(data[index]);
   }
 }
 
 template <typename Dtype>
-__global__ void kernel_channel_sum(const int num, const int channels,
+__global__ void kernel_channel_sum(hipLaunchParm lp, const int num, const int channels,
     const int spatial_dim, const Dtype* data, Dtype* channel_sum) {
-  CUDA_KERNEL_LOOP(index, num * spatial_dim) {
+  HIP_KERNEL_LOOP(index, num * spatial_dim) {
     int n = index / spatial_dim;
     int s = index % spatial_dim;
     Dtype sum = 0;
@@ -56,10 +54,10 @@ __global__ void kernel_channel_sum(const int num, const int channels,
 }
 
 template <typename Dtype>
-__global__ void kernel_channel_div(const int count,
+__global__ void kernel_channel_div(hipLaunchParm lp, const int count,
     const int num, const int channels,
     const int spatial_dim, const Dtype* channel_sum, Dtype* data) {
-  CUDA_KERNEL_LOOP(index, count) {
+  HIP_KERNEL_LOOP(index, count) {
     int n = index / channels / spatial_dim;
     int s = index % spatial_dim;
     data[index] /= channel_sum[n * spatial_dim + s];
@@ -67,10 +65,10 @@ __global__ void kernel_channel_div(const int count,
 }
 
 template <typename Dtype>
-__global__ void kernel_channel_dot(const int num, const int channels,
+__global__ void kernel_channel_dot(hipLaunchParm lp, const int num, const int channels,
     const int spatial_dim, const Dtype* data_1, const Dtype* data_2,
     Dtype* channel_dot) {
-  CUDA_KERNEL_LOOP(index, num * spatial_dim) {
+  HIP_KERNEL_LOOP(index, num * spatial_dim) {
     int n = index / spatial_dim;
     int s = index % spatial_dim;
     Dtype dot = 0;
@@ -95,27 +93,27 @@ void SoftmaxLayer<Dtype>::Forward_gpu(const vector<Blob<Dtype>*>& bottom,
   // and then normalize.
   // compute max
   // NOLINT_NEXT_LINE(whitespace/operators)
-  kernel_channel_max<Dtype><<<CAFFE_GET_BLOCKS(outer_num_ * inner_num_),
-      CAFFE_CUDA_NUM_THREADS>>>(outer_num_, channels, inner_num_, top_data,
+  hipLaunchKernel(HIP_KERNEL_NAME(kernel_channel_max<Dtype>), dim3(CAFFE_GET_BLOCKS(outer_num_ * inner_num_)),
+      dim3(CAFFE_HIP_NUM_THREADS), 0, 0, outer_num_, channels, inner_num_, top_data,
       scale_data);
   // subtract
   // NOLINT_NEXT_LINE(whitespace/operators)
-  kernel_channel_subtract<Dtype><<<CAFFE_GET_BLOCKS(count),
-      CAFFE_CUDA_NUM_THREADS>>>(count, outer_num_, channels, inner_num_,
+  hipLaunchKernel(HIP_KERNEL_NAME(kernel_channel_subtract<Dtype>), dim3(CAFFE_GET_BLOCKS(count)),
+      dim3(CAFFE_HIP_NUM_THREADS), 0, 0, count, outer_num_, channels, inner_num_,
       scale_data, top_data);
   // exponentiate
   // NOLINT_NEXT_LINE(whitespace/operators)
-  kernel_exp<Dtype><<<CAFFE_GET_BLOCKS(count), CAFFE_CUDA_NUM_THREADS>>>(
+  hipLaunchKernel(HIP_KERNEL_NAME(kernel_exp<Dtype>), dim3(CAFFE_GET_BLOCKS(count)), dim3(CAFFE_HIP_NUM_THREADS), 0, 0,
       count, top_data, top_data);
   // sum after exp
   // NOLINT_NEXT_LINE(whitespace/operators)
-  kernel_channel_sum<Dtype><<<CAFFE_GET_BLOCKS(outer_num_ * inner_num_),
-      CAFFE_CUDA_NUM_THREADS>>>(outer_num_, channels, inner_num_, top_data,
+  hipLaunchKernel(HIP_KERNEL_NAME(kernel_channel_sum<Dtype>), dim3(CAFFE_GET_BLOCKS(outer_num_ * inner_num_)),
+      dim3(CAFFE_HIP_NUM_THREADS), 0, 0, outer_num_, channels, inner_num_, top_data,
       scale_data);
   // divide
   // NOLINT_NEXT_LINE(whitespace/operators)
-  kernel_channel_div<Dtype><<<CAFFE_GET_BLOCKS(count),
-      CAFFE_CUDA_NUM_THREADS>>>(count, outer_num_, channels, inner_num_,
+  hipLaunchKernel(HIP_KERNEL_NAME(kernel_channel_div<Dtype>), dim3(CAFFE_GET_BLOCKS(count)),
+      dim3(CAFFE_HIP_NUM_THREADS), 0, 0,  count, outer_num_, channels, inner_num_,
       scale_data, top_data);
 }
 
@@ -131,12 +129,12 @@ void SoftmaxLayer<Dtype>::Backward_gpu(const vector<Blob<Dtype>*>& top,
   caffe_copy(count, top_diff, bottom_diff);
   // Compute inner1d(top_diff, top_data) and subtract them from the bottom diff.
   // NOLINT_NEXT_LINE(whitespace/operators)
-  kernel_channel_dot<Dtype><<<CAFFE_GET_BLOCKS(outer_num_ * inner_num_),
-      CAFFE_CUDA_NUM_THREADS>>>(outer_num_, channels, inner_num_,
+  hipLaunchKernel(HIP_KERNEL_NAME(kernel_channel_dot<Dtype>), dim3(CAFFE_GET_BLOCKS(outer_num_ * inner_num_)),
+      dim3(CAFFE_HIP_NUM_THREADS), 0, 0, outer_num_, channels, inner_num_,
       top_diff, top_data, scale_data);
   // NOLINT_NEXT_LINE(whitespace/operators)
-  kernel_channel_subtract<Dtype><<<CAFFE_GET_BLOCKS(count),
-      CAFFE_CUDA_NUM_THREADS>>>(count, outer_num_, channels, inner_num_,
+  hipLaunchKernel(HIP_KERNEL_NAME(kernel_channel_subtract<Dtype>), dim3(CAFFE_GET_BLOCKS(count)),
+      dim3(CAFFE_HIP_NUM_THREADS), 0, 0, count, outer_num_, channels, inner_num_,
       scale_data, bottom_diff);
   // elementwise multiplication
   caffe_gpu_mul<Dtype>(top[0]->count(), bottom_diff, top_data, bottom_diff);
