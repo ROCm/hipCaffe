28e96823e24b5fc718e34b30c5a8d5ca60aaeef6
Neelakandan <neelakandan@multicorewareinc.com>
2016-08-04 07:27:28 +0530

    Add lrn layer hip implementation

--- src/caffe/layers/lrn_layer_hip.cpp
+++ src/caffe/layers/lrn_layer_hip.cpp
@@ -6,11 +6,11 @@
 namespace caffe {
 
 template <typename Dtype>
-__global__ void LRNFillScale(const int nthreads, const Dtype* const in,
+__global__ void LRNFillScale(hipLaunchParm lp, const int nthreads, const Dtype* const in,
     const int num, const int channels, const int height,
     const int width, const int size, const Dtype alpha_over_size,
     const Dtype k, Dtype* const scale) {
-  CUDA_KERNEL_LOOP(index, nthreads) {
+  HIP_KERNEL_LOOP(index, nthreads) {
     // find out the local offset
     const int w = index % width;
     const int h = (index / width) % height;
@@ -69,9 +69,9 @@ void LRNLayer<Dtype>::Forward_gpu(const vector<Blob<Dtype>*>& bottom,
 
 // TODO: check if it would be faster to just put it into the previous kernel.
 template <typename Dtype>
-__global__ void LRNComputeOutput(const int nthreads, const Dtype* const in,
+__global__ void LRNComputeOutput(hipLaunchParm lp, const int nthreads, const Dtype* const in,
     const Dtype* const scale, const Dtype negative_beta, Dtype* const out) {
-  CUDA_KERNEL_LOOP(index, nthreads) {
+  HIP_KERNEL_LOOP(index, nthreads) {
     out[index] = in[index] * pow(scale[index], negative_beta);
   }
 }
@@ -87,15 +87,15 @@ void LRNLayer<Dtype>::CrossChannelForward_gpu(
   // go through all the channels.
   int n_threads = num_ * height_ * width_;
   // NOLINT_NEXT_LINE(whitespace/operators)
-  LRNFillScale<<<CAFFE_GET_BLOCKS(n_threads), CAFFE_CUDA_NUM_THREADS>>>(
+  hipLaunchKernel(HIP_KERNEL_NAME(LRNFillScale), dim3(CAFFE_GET_BLOCKS(n_threads)), dim3(CAFFE_HIP_NUM_THREADS), 0, 0,
       n_threads, bottom_data, num_, channels_, height_, width_, size_,
       alpha_ / size_, k_, scale_data);
-  CUDA_POST_KERNEL_CHECK;
+  //HIP_POST_KERNEL_CHECK;
   n_threads = bottom[0]->count();
   // NOLINT_NEXT_LINE(whitespace/operators)
-  LRNComputeOutput<<<CAFFE_GET_BLOCKS(n_threads), CAFFE_CUDA_NUM_THREADS>>>(
+  hipLaunchKernel(HIP_KERNEL_NAME(LRNComputeOutput), dim3(CAFFE_GET_BLOCKS(n_threads)), dim3(CAFFE_HIP_NUM_THREADS), 0, 0,
       n_threads, bottom_data, scale_data, -beta_, top_data);
-  CUDA_POST_KERNEL_CHECK;
+  //HIP_POST_KERNEL_CHECK;
 }
 template void LRNLayer<float>::CrossChannelForward_gpu(
     const vector<Blob<float>*>& bottom, const vector<Blob<float>*>& top);
@@ -119,13 +119,13 @@ void LRNLayer<Dtype>::Backward_gpu(const vector<Blob<Dtype>*>& top,
 }
 
 template <typename Dtype>
-__global__ void LRNComputeDiff(const int nthreads,
+__global__ void LRNComputeDiff(hipLaunchParm lp, const int nthreads,
     const Dtype* const bottom_data, const Dtype* const top_data,
     const Dtype* const scale, const Dtype* const top_diff,
     const int num, const int channels, const int height,
     const int width, const int size, const Dtype negative_beta,
     const Dtype cache_ratio, Dtype* const bottom_diff) {
-  CUDA_KERNEL_LOOP(index, nthreads) {
+  HIP_KERNEL_LOOP(index, nthreads) {
     // find out the local offset
     const int w = index % width;
     const int h = (index / width) % height;
@@ -182,7 +182,7 @@ void LRNLayer<Dtype>::CrossChannelBackward_gpu(
     const vector<Blob<Dtype>*>& bottom) {
   int n_threads = num_ * height_ * width_;
   // NOLINT_NEXT_LINE(whitespace/operators)
-  LRNComputeDiff<<<CAFFE_GET_BLOCKS(n_threads), CAFFE_CUDA_NUM_THREADS>>>(
+  hipLaunchKernel(HIP_KERNEL_NAME(LRNComputeDiff), dim3(CAFFE_GET_BLOCKS(n_threads)), dim3(CAFFE_HIP_NUM_THREADS), 0, 0,
       n_threads, bottom[0]->gpu_data(), top[0]->gpu_data(),
       scale_.gpu_data(), top[0]->gpu_diff(), num_, channels_, height_, width_,
       size_, -beta_, Dtype(2. * alpha_ * beta_ / size_),
