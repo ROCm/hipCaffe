bb6788618b6e9bdc6baa7be31f22bf84507005e9
Wen-Heng (Jack) Chung <whchung@gmail.com>
2016-12-02 23:15:35 +0800

    [MIOPEN] tanh layer integration logic

--- include/caffe/layers/cudnn_tanh_layer.hpp
+++ include/caffe/layers/cudnn_tanh_layer.hpp
@@ -35,7 +35,10 @@ class CuDNNTanHLayer : public TanHLayer<Dtype> {
 
   bool handles_setup_;
 #ifdef USE_MIOPEN
-  // TBD
+  mlopenHandle_t             handle_;
+  mlopenTensorDescriptor_t bottom_desc_;
+  mlopenTensorDescriptor_t top_desc_;
+  mlopenActivationDescriptor_t activ_desc_;
 #endif
 
 #ifdef USE_CUDNN
--- src/caffe/layers/cudnn_tanh_layer.cpp
+++ src/caffe/layers/cudnn_tanh_layer.cpp
@@ -10,7 +10,11 @@ void CuDNNTanHLayer<Dtype>::LayerSetUp(const vector<Blob<Dtype>*>& bottom,
       const vector<Blob<Dtype>*>& top) {
   TanHLayer<Dtype>::LayerSetUp(bottom, top);
 #ifdef USE_MIOPEN
-  // TBD
+  // initialize MIOpen
+  MIOPEN_CHECK(mlopenCreate(&handle_));
+  miopen::createTensor4dDesc<Dtype>(&bottom_desc_);
+  miopen::createTensor4dDesc<Dtype>(&top_desc_);
+  miopen::createActivationDescriptor<Dtype>(&activ_desc_, mlopenActivationTANH);
 #endif
 
 #ifdef USE_CUDNN
@@ -19,8 +23,9 @@ void CuDNNTanHLayer<Dtype>::LayerSetUp(const vector<Blob<Dtype>*>& bottom,
   cudnn::createTensor4dDesc<Dtype>(&bottom_desc_);
   cudnn::createTensor4dDesc<Dtype>(&top_desc_);
   cudnn::createActivationDescriptor<Dtype>(&activ_desc_, CUDNN_ACTIVATION_TANH);
-  handles_setup_ = true;
 #endif
+
+  handles_setup_ = true;
 }
 
 template <typename Dtype>
@@ -32,7 +37,8 @@ void CuDNNTanHLayer<Dtype>::Reshape(const vector<Blob<Dtype>*>& bottom,
   const int H = bottom[0]->height();
   const int W = bottom[0]->width();
 #ifdef USE_MIOPEN
-  // TBD
+  miopen::setTensor4dDesc<Dtype>(&bottom_desc_, N, K, H, W);
+  miopen::setTensor4dDesc<Dtype>(&top_desc_, N, K, H, W);
 #endif
 
 #ifdef USE_CUDNN
@@ -47,7 +53,9 @@ CuDNNTanHLayer<Dtype>::~CuDNNTanHLayer() {
   if (!handles_setup_) { return; }
 
 #ifdef USE_MIOPEN
-  // TBD
+  mlopenDestroyTensorDescriptor(this->bottom_desc_);
+  mlopenDestroyTensorDescriptor(this->top_desc_);
+  mlopenDestroy(this->handle_);
 #endif
 
 #ifdef USE_CUDNN
