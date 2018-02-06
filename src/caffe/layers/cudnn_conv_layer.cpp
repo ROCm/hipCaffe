bbeb2196aa7af9d52b26326f4cfa2f3156dc3a56
Wen-Heng (Jack) Chung <whchung@gmail.com>
2018-02-06 00:07:32 +0000

    XXXX show NCHW info

--- src/caffe/layers/cudnn_conv_layer.cpp
+++ src/caffe/layers/cudnn_conv_layer.cpp
@@ -348,6 +348,7 @@ void CuDNNConvolutionLayer<Dtype>::Reshape(
     ));
 
     fwd_algo_[i] = perf.fwd_algo;
+    LOG(INFO) << "(N, C, H, W): (" << N_ << ", " << C_ << ", " << H_ << ", " << W_ << ")";
     LOG(INFO) << " - fwd_algo_[" << i << "]:        " << fwd_algo_[i];
     DLOG(INFO) << "workspace_fwd_sizes_[" << i << "]:" << workspace_fwd_sizes_[i] << "\n";
 #endif
